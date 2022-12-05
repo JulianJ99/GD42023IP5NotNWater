dc77dc6ec27f5ff0974965c60d5405857b4557d6
JulianJ99 <465268@student.fontys.nl>
2022-12-05 16:42:21 +0100

    ShowerTimer class changes

--- WaterYouDoing-main/Water You Doing/Assets/Scripts/ShowerTimer/ShowerTimer.cs
+++ WaterYouDoing-main/Water You Doing/Assets/Scripts/ShowerTimer/ShowerTimer.cs
@@ -3,19 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ShowerTimer : MonoBehaviour
 {
-    [SerializeField] private Vector3 h_endPoint = Vector3.zero;
-    [SerializeField] private Vector3 v_endPoint = Vector3.zero;
+
     private Vector3 startPosition = Vector3.zero;
     private Vector3 endVerticalPosistion = Vector3.zero;
     private Vector3 objPosition = Vector3.zero;
     public Sprite open;
 
+    private bool horizontalMove = true;
+    private bool isTouched = false;
 
-    public float verticalMoveSpeed = 1f;
-    public float horizontalMoveSpeed = 0.5f;
+    public float timer;
 
     //used for scaling difficulty
     float difficulty;
@@ -49,7 +51,6 @@ public class ShowerTimer : MonoBehaviour
         difficulty = GlobalGameManager.Instance.difficulty;
 
         this.startPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-        this.h_endPoint = this.GetRelativeEndpoint();
     }
 
     // Update is called once per frame
@@ -58,12 +59,20 @@ public class ShowerTimer : MonoBehaviour
         //If the screen's touched, the shower curtains will open, "ending" the game
         if (isTouched)
         {
-            if(timer = <5f){
+            if((timer <= 6f) || (timer >= 4f) ){
                 GlobalGameManager.Instance.WinGame();
             }
-            else(){
+            else{
                 GlobalGameManager.Instance.LoseGame();
             }
+        }
+
+        if (timer <= 8f){
+            //Fog 1
+        }
+
+        if (timer <= 6f){
+            //Fog 2
         }
 
     }
@@ -80,8 +89,6 @@ public class ShowerTimer : MonoBehaviour
             else
             {
                 isTouched = true;
-                this.objPosition = this.GetObjectPosition();
-                this.endVerticalPosistion = this.GetRelativeVerticalEndpoint();
             }
         }
     }
